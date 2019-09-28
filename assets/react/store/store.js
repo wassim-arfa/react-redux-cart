676ce7697fca4ac050183ccbabd04cf59ebed48f
Wassim Arfa <wassim.arfa@gmail.com>
2019-09-28 10:59:03 +0100

    REDUX_DEVTOOLS

--- assets/react/store/store.js
+++ assets/react/store/store.js
@@ -29,7 +29,7 @@ const initialState = {
     ]
 }
 
-const store = createStore(cartReducer, initialState);
+const store = createStore(cartReducer, initialState,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
 
