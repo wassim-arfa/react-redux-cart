b5243dd81c6581bcb1e4ba4a882a34132b7b4424
Wassim Arfa <wassim.arfa@gmail.com>
2019-10-02 12:51:20 +0100

    reducer: REMOVE_FROM_CART

--- assets/react/components/CartItem.js
+++ assets/react/components/CartItem.js
@@ -1,24 +1,44 @@
 import React from 'react'
+import {removeFromCart} from "../store/actions/actions";
+import {connect} from "react-redux";
 
-export default function CartItem(props) {
+class CartItem extends React.Component {
 
-    const {product} = props
-    return (
- 
-        <div className="card mr-3 mb-3 box-shadow" style={{width: '10rem'}}>
-            <img className="card-img-top" alt={product.title}
-                 style={{height: 100, width: '100%', display: 'block'}}
-                 src={product.picture}
-                 data-holder-rendered="true"/>
+    removeFromCart = (product) => {
+        this.props.removeFromCart(product);
+    }
+    
+    render() {
+        const {
+            props,
+        } = this;
 
-            <div className="card-body">
-                <h5 className="card-title">{product.title}</h5>
-                <p className="card-text">Price : {product.price} $</p>
-                <p className="card-text">Quantity : {props.quantity}</p>
-                <p className="card-text">Total price : {props.quantity * product.price}</p>
-                <button className="btn btn-danger">Delete</button>
+        const {product} = props
+        return (
+     
+            <div className="card mr-3 mb-3 box-shadow" style={{width: '10rem'}}>
+                <img className="card-img-top" alt={product.title}
+                     style={{height: 100, width: '100%', display: 'block'}}
+                     src={product.picture}
+                     data-holder-rendered="true"/>
+
+                <div className="card-body">
+                    <h5 className="card-title">{product.title}</h5>
+                    <p className="card-text">Price : {product.price} $</p>
+                    <p className="card-text">Quantity : {props.quantity}</p>
+                    <p className="card-text">Total price : {props.quantity * product.price}</p>
+                    <button className="btn btn-danger" onClick={() => props.removeFromCart(product)}>Delete</button>
+                </div>
             </div>
-        </div>
 
-    )
+        )
+    }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        removeFromCart: (product) => dispatch(removeFromCart(product)),
+    };
+}
+
+export default connect(null, mapDispatchToProps)(CartItem);
--- assets/react/store/reducers/index.js
+++ assets/react/store/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../actions/types";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/types";
 
 export default function cartReducer(state, action){
 console.log(state, action)
@@ -6,6 +6,7 @@ console.log(state, action)
     switch(action.type){
         case ADD_TO_CART: {
             return {
+                ...state,
                 cart: [
                     ...state.cart,
                     {
@@ -15,7 +16,15 @@ console.log(state, action)
                 ]
             }
         }
-
+        case REMOVE_FROM_CART: {
+            console.log(action.product.id, 'deleted')
+            const newCart = state.cart.filter(cartItem=>cartItem.product.id != action.product.id)
+            // console.log(newCart)
+            return {
+                ...state,
+                cart: newCart
+            }
+        }
         default:
             return state;
     }
